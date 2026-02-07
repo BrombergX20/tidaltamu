6abc748ca46ee3b70e21d6b24ae97982b6a2ae7a
AshTree007 <ashwin.gdr1@gmail.com>
2026-02-07 16:52:12 -0600

    added file upload functionality to api.py, and added temp/ to .gitignore

--- api.py
+++ api.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
+import os
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Starting...")
+    os.makedirs("temp/files/", exist_ok=True)
+    os.makedirs("temp/videos/", exist_ok=True)
+    os.makedirs("temp/audios/", exist_ok=True)
     yield
     print("Shutdown")
 
@@ -23,14 +27,24 @@ app.add_middleware(
 async def add_doc(file: UploadFile = File(...), type: str = Form(...) ):
     if type in ["pdf", "docx", "txt"]:
         print("type: ", type)
-        return {"message": f"Document added successfully, type: {type}"}
+        save_path = f"temp/files/{file.filename}"
+        with open(save_path, "wb") as f:
+            f.write(await file.read())
+        return {"message": f"Document added successfully, type: {type}", "path": save_path}
     
     elif type in ["mp4", "avi", "mkv"]:
         print("type: ", type)
-        return {"message": f"Video added successfully, type: {type}"}
+        save_path = f"temp/videos/{file.filename}"
+        with open(save_path, "wb") as f:
+            f.write(await file.read())
+        return {"message": f"Video added successfully, type: {type}", "path": save_path}
     
     elif type in ["mp3", "wav", "aac"]:
         print("type: ", type)
-        return {"message": f"Audio added successfully, type: {type}"}
+        save_path = f"temp/audios/{file.filename}"
+        with open(save_path, "wb") as f:
+            f.write(await file.read())
+        return {"message": f"Audio added successfully, type: {type}", "path": save_path}
 
     return {"message": "Unknown file type"}
+
---
+++ db_upload.py
@@ -0,0 +1,4 @@
+import boto3
+import json
+
+s3 = boto3.client('s3')
