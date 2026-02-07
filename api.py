e67b5322323a7534ef9b73f80692b12d3b2a7dd9
AshTree007 <ashwin.gdr1@gmail.com>
2026-02-07 16:05:35 -0600

    added to backend api

--- api.py
+++ api.py
@@ -1,11 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
 from contextlib import asynccontextmanager
-from pydantic import BaseModel
-
-class file_upload(BaseModel):
-    url: str
-    type: str
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,22 +7,17 @@ async def lifespan(app: FastAPI):
     yield
     print("Shutdown")
 
-
 app = FastAPI(lifespan=lifespan)
 
 @app.post("/add_doc")
-def add_doc(doc: file_upload):
+async def add_doc(file: UploadFile = File(...), type: str = Form(...) ):
+    if type in ["pdf", "docx", "txt"]:
+        return {"message": f"Document added successfully, type: {type}"}
+    
+    elif type in ["mp4", "avi", "mkv"]:
+        return {"message": f"Video added successfully, type: {type}"}
+    
+    elif type in ["mp3", "wav", "aac"]:
+        return {"message": f"Audio added successfully, type: {type}"}
 
-    if doc.type in ["pdf", "docx", "txt"]:
-        return {"message": f"Document added successfully, type: {doc.type}"}
-    
-    elif doc.type in ["mp4", "avi", "mkv"]:
-        return {"message": f"Video added successfully, type: {doc.type}"}
-    
-    elif doc.type in ["mp3", "wav", "aac"]:
-        return {"message": f"Audio added successfully, type: {doc.type}"}
-    
-
-if __name__ == "__main__":
-    file = file_upload(url="http://example.com/document.pdf", type="")
-    print(add_doc(file))
+    return {"message": "Unknown file type"}
