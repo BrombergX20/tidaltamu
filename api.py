019247631d373254e847d103be5316984b0c8601
AshTree007 <ashwin.gdr1@gmail.com>
2026-02-07 15:35:51 -0600

    api update

--- api.py
+++ api.py
@@ -1,7 +1,34 @@
-import fastapi
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from pydantic import BaseModel
+
+class file_upload(BaseModel):
+    url: str
+    type: str
 
 
+@asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Starting...")
     yield
-    print("Stopped.")
+    print("Shutdown")
+
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/add_doc")
+def add_doc(doc: file_upload):
+
+    if doc.type in ["pdf", "docx", "txt"]:
+        return {"message": f"Document added successfully, type: {doc.type}"}
+    
+    elif doc.type in ["mp4", "avi", "mkv"]:
+        return {"message": f"Video added successfully, type: {doc.type}"}
+    
+    elif doc.type in ["mp3", "wav", "aac"]:
+        return {"message": f"Audio added successfully, type: {doc.type}"}
+    
+
+if __name__ == "__main__":
+    file = file_upload(url="http://example.com/document.pdf", type="")
+    print(add_doc(file))
