b57b69eb529093679ca1a94e81daec0ac025b8f3
BrombergX20 <xanderbromberg@gmail.com>
2026-02-07 18:38:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // const API_BASE = (typeof window !== 'undefined' && (window.API_BASE || window.__API_BASE__)) ? (window.API_BASE || window.__API_BASE__) : '';
-const API_BASE = "ec2-44-252-41-71.us-west-2.compute.amazonaws.com"
+const API_BASE = "http://ec2-44-252-41-71.us-west-2.compute.amazonaws.com/upload"
+
 
 
 async function saveFileToServer(file){
