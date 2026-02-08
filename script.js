493577114e38e2510d2b6d9aeedaa4f9160efd3c
BrombergX20 <xanderbromberg@gmail.com>
2026-02-07 18:40:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // const API_BASE = (typeof window !== 'undefined' && (window.API_BASE || window.__API_BASE__)) ? (window.API_BASE || window.__API_BASE__) : '';
-const API_BASE = "http://ec2-44-252-41-71.us-west-2.compute.amazonaws.com/upload"
+const API_BASE = "https://ec2-44-252-41-71.us-west-2.compute.amazonaws.com/upload"
 
 
 
